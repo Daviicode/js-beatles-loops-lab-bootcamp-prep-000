6499e03094902a878f039abe54fb04caff889290
David zhang <d.ydzdavid@yahoo.com>
2018-12-27 12:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ const fact = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  "He hated the sound of his own voice"];
 
 function johnLennonFacts (facts) {
   var i = 0;
